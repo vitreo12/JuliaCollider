b8affae977512e2949f90d259a577774043dad2f
Unknown <francesco.cameli95@gmail.com>
2019-03-07 19:35:23 +0100

    Updated jl_get_module, jl_get_global and jl_get_module_in_main macros
    Former-commit-id: 5bf50a8350fdb5422400ef343236005680075200 [formerly a019b6b3801ca3c777eb5bf7cad06fe11b2833b3]
    Former-commit-id: 0d6f5c58481bb2d573064fab1ce0e64de430b842

--- src/JuliaUtilities.hpp
+++ src/JuliaUtilities.hpp
@@ -3,7 +3,11 @@
 
 #pragma once
 
-#define jl_get_module(name) (jl_module_t*)jl_get_global(jl_main_module, jl_symbol(name))
+#define jl_get_module_in_main(name) (jl_module_t*)jl_get_global(jl_main_module, jl_symbol(name))
+
+#define jl_get_module(module, name) (jl_module_t*)jl_get_global(module, jl_symbol(name))
+
+#define jl_get_global_SC(module, name) jl_get_global(module, jl_symbol(name))
 
 //This is the same as jl_call, but it doesn't perform the GC pushing and popping, since
 //it will be called on objects that I know already that won't be picked up by
